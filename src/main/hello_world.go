d56bae7c8d8c26870aea126b2add31499d7a42ef
Diwakar Timilsina <diwakar.timilsina@gmail.com>
2019-01-20 23:23:30 -0600

    changes

--- src/main/hello_world.go
+++ src/main/hello_world.go
@@ -2,35 +2,16 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-	"time"
+	"service"
 )
 
 func main() {
 	cmd := "curl"
 	args := []string{"google.com"}
 
-	for i := 0; i < 500; i++ {
-		go fetchURL(cmd, args)
+	for i := 0; i < 5; i++ {
+		go service.FetchURL(cmd, args)
 	}
 	fmt.Scanln()
 	fmt.Println("done")
-}
-
-func fetchURL(cmd string, args []string) {
-	var (
-		err    error
-		cmdOut []byte
-	)
-
-	fmt.Println(time.Now())
-	fmt.Println("Command: " + cmd)
-
-	cmdOut, err = exec.Command(cmd, args...).Output()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	fmt.Println("I am here")
-	fmt.Println(string(cmdOut))
 }
